04a3f221d43b0503d8efebfb09063ede043defa7
Nathan Arnold <nathan@nyamedia.com>
2013-07-26 21:09:34 +0000

    added usage details to README, and cleaned up some variable names in the JS file

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ A jQuery plugin for the display of 'responsive design' images via CSS background
 
 Feel free to clone and use at your own risk.
 
+Basic Usage:
+
+Attach a 'data-rImg="<filename>"' attribute to any span tags you'd like to use as an image. There are two tokens that are necessary, ?ratio? for the image ratio, and ?size? for the image size. Put these in your src filepath and make that the value for the 'data-rImg' attribute. Eg. <span data-rimg="img/?ratio?/?size?.jpg" />
+
+Add a 'data-rimg-ratio' attribute to define the ratio of the image you'd like to use. Eg. '4x3' or '16x9'
+
+Add a 'data-rimg-sizes' attribute to define the various @media breakpoints (and corresponding image size values) you'd like to use. Add these in '-' seperated pairs (eg. 320-320 for @media (min-width:320px) and an image 320px wide). Use the 'x2' extension for retina display images (eg. 320x2-320 for @media (min-width:320px) and (-webkit-min-device-pixel-ratio: 1.5) and an image 320px wide). all query/size pairs should be comma-separated. Eg: data-img-sizes="320-320,320x-640,640-640,640x2-1280"
+
+See the example-js.html page as an example of this in action. 
+
--- rImg.js
+++ rImg.js
@@ -34,13 +34,14 @@
 			},
 			
 			writeCSS: function( $this ) {
-				// get the background attributes we need
+				// get the attributes we need
 				var imgId = $this.attr('id');
-				var src   = $this.attr('data-rimg');
-				var ratio = $this.attr('data-rimg-ratio');
-				var sizes = $this.attr('data-rimg-sizes').split(',');
+				var imgSrc   = $this.attr('data-rimg');
+				var imgRatio = $this.attr('data-rimg-ratio');
+				var imgSizes = $this.attr('data-rimg-sizes').split(',');
 
-				// if there is no ID, we should give it one
+				// if there is no ID, we should give it one so we can target it with CSS
+				// ??? Is this smart? Should we use a unique className instead ???
 				if ( !imgId ) {
 					imgId = 'rImg-' + Math.floor((Math.random()*10000000)+1);
 					$this.attr('id', imgId);
@@ -48,8 +49,8 @@
 
 				// figure out what sizes we need
 				var sizeData = [], tmp = [], strTmp = '';
-                for (var i=0; i<sizes.length; i++) {
-					tmp = sizes[i].split('-');
+				for (var i=0; i<imgSizes.length; i++) {
+					tmp = imgSizes[i].split('-');
 					if ( tmp.length == 1) {
 						sizeData.push( [ tmp[0], tmp[0], false ] );
 					} else {
@@ -61,29 +62,22 @@
 							sizeData.push( [ parseInt( tmp[0] ), parseInt( tmp[1] ), false ] );
 					};
 				};
-				console.log(sizeData);
-
-
 
 				// declare string vars we'll need
 				var x2Query = '';
-				//var imgSrc = src.replace("?size?",  sizes[0]);
-				//	imgSrc = imgSrc.replace("?ratio?", ratio);
-				var imgSrc = methods.getUrl( src, sizeData[0][1], ratio );
+				var src = methods.getUrl( imgSrc, sizeData[0][1], imgRatio );
 
 				// define base style declaration
-				var html =  '#'+ imgId +' {background-image:url("'+ imgSrc +'")}\n';
+				var html =  '#'+ imgId +' {background-image:url("'+ src +'")}\n';
 				
 				// subsequent @media queries
-				for (var i=1; i<sizes.length; i++) {
+				for (var i=1; i<imgSizes.length; i++) {
 					// image size
-					//imgSrc = src.replace("?size?", sizeData[i][1]);
-					//imgSrc = imgSrc.replace("?ratio?", ratio);
-					imgSrc = methods.getUrl( src, sizeData[i][1], ratio );
+					src = methods.getUrl( imgSrc, sizeData[i][1], imgRatio );
 					// retina display extension
 					x2Query = ( sizeData[i][2] ) ? 'and (-webkit-min-device-pixel-ratio: 1.5)' : '';
 					// style declaration
-					html += '@media (min-width:'+ sizeData[i][0] +'px) '+ x2Query +' { #'+ imgId +' {background-image:url("'+ imgSrc +'")}  }\n';
+					html += '@media (min-width:'+ sizeData[i][0] +'px) '+ x2Query +' { #'+ imgId +' {background-image:url("'+ src +'")}  }\n';
 				};
 				html += '\n';
 				
